a515d3b48354bb1a10a43efe12585e1c0f00c30d
DONGJIAYUN932 <dongjiayun932@pingan.com.cn>
2024-06-19 16:20:04 +0800

    自动检查更新

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
+import { useLaunch,useDidShow } from '@tarojs/taro'
 import '@nutui/nutui-react-taro/dist/style.css'
 
 import './app.scss'
@@ -8,6 +8,22 @@ function App({ children }: PropsWithChildren<any>) {
 
     useLaunch(() => {
         console.log('App launched.')
+    })
+
+    useDidShow(()=>{
+        const updateManager = Taro.getUpdateManager()
+        updateManager.onUpdateReady(function () {
+            Taro.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success: function (res) {
+                    if (res.confirm) {
+                        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                        updateManager.applyUpdate()
+                    }
+                }
+            })
+        })
     })
 
     // children 是将要会渲染的页面
